44e73720d88f27ac4fb71b53f2a396971e9f19e9
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-01-07 17:07:11 +0100

    managing when a piece tries to go out of bounds

---
+++ src/main/java/OutOfBoundsException.java
@@ -0,0 +1,8 @@
+public class OutOfBoundsException extends Exception{
+    public OutOfBoundsException(){
+        super("Out of Bounds");
+    }
+    public OutOfBoundsException(String message){
+        super(message);
+    }
+}
