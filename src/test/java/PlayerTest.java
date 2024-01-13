2609c0e3aeb045a23f7caf0c8e8d085e13078942
Simone Divo <105349008+simonedivo@users.noreply.github.com>
2024-01-13 11:26:36 +0100

    Update PlayerTest.java

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,13 +1,2 @@
-import Exceptions.NoPieceOnWhiteException;
-
 public class PlayerTest {
-    Player player;
-    {
-        try {
-            player = new Player("Dracula", Team.White);
-        } catch (NoPieceOnWhiteException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
