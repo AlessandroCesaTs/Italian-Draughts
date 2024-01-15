f575e2fae27db4a74aa35d260b5b58eea2be1cdf
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-01-15 12:38:31 +0100

    Moved origin of Jpanel (Board) from top left to bottom left; and changed colors of board accordingly

--- src/main/java/gui/GraphicBoard.java
+++ src/main/java/gui/GraphicBoard.java
@@ -21,12 +21,12 @@ public class GraphicBoard extends JPanel {
         for (int c=0; c < cols; c++) {
             for (int r = 0; r <= 2; r++) {
                 if ((r+c) % 2 != 0){
-                    pieceList.add(new NormalPiece(this, c, r, false));
+                    pieceList.add(new NormalPiece(this, c, r, true));
                 }
             }
             for (int r = 5; r < rows; r++) {
                 if ((r+c) % 2 != 0){
-                    pieceList.add(new NormalPiece(this, c, r, true));
+                    pieceList.add(new NormalPiece(this, c, r, false));
                 }
             }
         }
@@ -35,9 +35,14 @@ public class GraphicBoard extends JPanel {
     public void paintComponent(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
 
+        // Translate the origin to the bottom left
+        g2d.translate(0, getHeight());
+        g2d.scale(1, -1);
+
+
         for (int r=0; r < rows; r++) {
             for (int c=0; c < cols; c++) {
-                g2d.setColor((c+r) %2 ==0 ? new Color(229, 183, 145, 255) : new Color(132, 84, 35));
+                g2d.setColor((c+r) %2 ==0 ? new Color(132, 84, 35) : new Color(229, 183, 145, 255));
                 g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
             }
         }
