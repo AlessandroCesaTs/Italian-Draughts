b5a6841be0c122d05a69fea8699ee9715283cded
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-02-02 10:24:42 +0100

    Tested player methods and solved issues in the creation of Player, related to how getFullBlackTiles worked

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,13 +1,37 @@
 import Exceptions.IllegalTilePlacementException;
 import Exceptions.NoPieceOnWhiteException;
 import logic.Game;
+import logic.Player;
+import logic.Team;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
-    Game game=new Game("Anna","Mario");
+    Game game=new Game("Anna","Mario", Team.White,Team.Black);
 
     public GameTest() throws IllegalTilePlacementException, NoPieceOnWhiteException {
+    }
+    Player player1=game.getActivePlayer();
+    Player player2=game.getActivePlayer();
+
+
+    @Test
+    void changeActivePlayerTest(){
+        game.changeActivePlayer();
+        Player active=game.getActivePlayer();
+        Player inactive=game.getInactivePlayer();
+        assertEquals("Mario",active.getName());
+        assertEquals("Anna",inactive.getName());
+    }
+    @Test
+    void losePieceTest(){
+        game.changeActivePlayer();
+        Player active=game.getActivePlayer();
+        Player inactive=game.getInactivePlayer();
+        assertEquals("Mario",active.getName());
+        assertEquals("Anna",inactive.getName());
     }
 
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,45 @@
+import Exceptions.IllegalTilePlacementException;
+import Exceptions.NoPieceOnWhiteException;
+import logic.Game;
+import logic.Player;
+import logic.Team;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class PlayerTest {
+
+    Game game=new Game("Anna","Mario", Team.White,Team.Black);
+
+    public PlayerTest() throws IllegalTilePlacementException, NoPieceOnWhiteException {
+    }
+
+    Player player1=game.getActivePlayer();
+    Player player2=game.getInactivePlayer();
+
+    @Test
+    void player1Test(){
+        assertEquals("Anna",player1.getName());
+        assertEquals(Team.White,player1.getTeam());
+        assertEquals(12,player1.getNumberOfPieces());
+    }
+    @Test
+    void player2Test(){
+        assertEquals("Mario",player2.getName());
+        assertEquals(Team.Black,player2.getTeam());
+        assertEquals(12,player2.getNumberOfPieces());
+    }
+
+    @Test
+    void playerEqualsTest1(){
+        Player testPlayer=game.getActivePlayer();
+        assertTrue(player1.equals(testPlayer));
+    }
+    @Test
+    void playerEqualsTest2(){
+        Player testPlayer=game.getInactivePlayer();
+        assertTrue(player2.equals(testPlayer));
+    }
 }
