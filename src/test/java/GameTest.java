cceee935d2a314ee7378c119fe93b6146a3adb77
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-02-19 12:34:44 +0100

    correctly testing 2 moves

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -34,4 +34,23 @@ public class GameTest {
         assertNull(board.getPiece(2,2));
     }
 
+    @Test
+    void moveTwoPiecesTest() throws NoPieceOnWhiteException, CantEatException, IllegalMovementException, OutOfBoundsException, NotOnDiagonalException {
+        Board board=game.getBoard();
+
+        Player player1=game.getActivePlayer();
+        Piece piece1=board.getPiece(2,2);
+        game.playTurn(new Move(player1,piece1,NeighborPosition.TopRight));
+
+        Player player2=game.getActivePlayer();
+        Piece piece2=board.getPiece(5,1);
+        game.playTurn(new Move(player2,piece2,NeighborPosition.BottomLeft));
+
+        assertEquals(piece1, BlackTile.asBlackTile(board.getTile(3,3)).getPiece());
+        assertNull(board.getPiece(2,2));
+        assertEquals(piece2,BlackTile.asBlackTile(board.getTile(4,0)).getPiece());
+        assertNull(board.getPiece(5,1));
+
+    }
+
 }
