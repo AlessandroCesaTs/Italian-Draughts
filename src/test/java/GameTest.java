c39651e478b36b204fe110ab25e752fd11d0a4e5
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-02-19 12:39:22 +0100

    testing illegalMove

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -7,16 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
 
     MockGame game =new MockGame("Anna","Mario", Team.White,Team.Black);
+    Board board=game.getBoard();
+    Player player1=game.getActivePlayer();
+    Player player2=game.getInactivePlayer();
 
     public GameTest() throws IllegalTilePlacementException, NoPieceOnWhiteException, IllegalTeamsCompositionException, CantEatException, IllegalMovementException, OutOfBoundsException, NotOnDiagonalException {
     }
     @Test
     void changeActivePlayerTest(){
         game.changeActivePlayer();
-        Player active= game.getActivePlayer();
-        Player inactive= game.getInactivePlayer();
-        assertEquals("Mario",active.getName());
-        assertEquals("Anna",inactive.getName());
+        assertEquals("Mario",player2.getName());
+        assertEquals("Anna",player1.getName());
     }
     @Test
     void illegalTeamCompositionTest(){
@@ -25,10 +26,8 @@ public class GameTest {
 
     @Test
     void moveOnePieceTest() throws NoPieceOnWhiteException, OutOfBoundsException, CantEatException, IllegalMovementException, NotOnDiagonalException {
-        Player player=game.getActivePlayer();
-        Board board=game.getBoard();
         Piece piece=board.getPiece(2,2);
-        Move move=new Move(player,piece,NeighborPosition.TopRight);
+        Move move=new Move(player1,piece,NeighborPosition.TopRight);
         game.playTurn(move);
         assertEquals(piece, BlackTile.asBlackTile(board.getTile(3,3)).getPiece());
         assertNull(board.getPiece(2,2));
@@ -36,13 +35,9 @@ public class GameTest {
 
     @Test
     void moveTwoPiecesTest() throws NoPieceOnWhiteException, CantEatException, IllegalMovementException, OutOfBoundsException, NotOnDiagonalException {
-        Board board=game.getBoard();
-
-        Player player1=game.getActivePlayer();
         Piece piece1=board.getPiece(2,2);
         game.playTurn(new Move(player1,piece1,NeighborPosition.TopRight));
 
-        Player player2=game.getActivePlayer();
         Piece piece2=board.getPiece(5,1);
         game.playTurn(new Move(player2,piece2,NeighborPosition.BottomLeft));
 
@@ -50,7 +45,13 @@ public class GameTest {
         assertNull(board.getPiece(2,2));
         assertEquals(piece2,BlackTile.asBlackTile(board.getTile(4,0)).getPiece());
         assertNull(board.getPiece(5,1));
+    }
 
+    @Test
+    void illegalMove() throws NoPieceOnWhiteException, IllegalMovementException, OutOfBoundsException, CantEatException, NotOnDiagonalException {
+        Piece piece=board.getPiece(7,5);
+        game.playTurn(new Move(player1,piece,NeighborPosition.BottomLeft));
+        assertEquals(piece,BlackTile.asBlackTile(board.getTile(7,5)).getPiece());
     }
 
 }
