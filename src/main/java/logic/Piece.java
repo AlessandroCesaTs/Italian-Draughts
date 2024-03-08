3be03afa34af2bce69a9da17c3952afa52259dbb
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-03-08 14:58:51 +0100

    deleted debug print

--- src/main/java/logic/Piece.java
+++ src/main/java/logic/Piece.java
@@ -121,7 +121,6 @@ public class Piece {
 
         for (NeighborPosition position : neighborPositions) {
             if(canEat(position)){
-                System.out.println(position);
                 return true;
             }
         }
